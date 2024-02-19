f90fe04535368f2467bd0f60d2d2966f082bd68f
2017tmxkxm <2017tmxkxm@gmail.com>
2024-02-19 14:42:02 +0900

    [Update] addForm, addItem, @ModelAttribute("openYn) 추가

--- market/src/main/java/com/free/market/item/controller/ItemController.java
+++ market/src/main/java/com/free/market/item/controller/ItemController.java
@@ -1,15 +1,23 @@
 package com.free.market.item.controller;
 
 import com.free.market.item.domain.Item;
+import com.free.market.item.domain.ItemSaveForm;
 import com.free.market.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -19,10 +27,38 @@ public class ItemController {
 
     private final ItemService itemService;
 
+    @ModelAttribute("openYn")
+    public Map<String, String> openYn() {
+        Map<String, String> openYn = new LinkedHashMap<>();
+        openYn.put("Y", "판매가능");
+        openYn.put("N", "판매불가");
+        return openYn;
+    }
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemService.findAll();
         model.addAttribute("items", items);
         return "item/items";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+        return "item/addForm";
+    }
+
+    @PostMapping("/add")
+    public String addItem(@Validated @ModelAttribute("item") ItemSaveForm form, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if(bindingResult.hasErrors()) {
+            return "item/addForm";
+        }
+
+        Long itemId = itemService.save(form);
+
+        redirectAttributes.addAttribute("itemId", itemId);
+
+        return "redirect:/item/{itemId}";
     }
 }
