e11d485f96512f4cf11b7085965c47102f6979f6
2017tmxkxm <2017tmxkxm@gmail.com>
2024-03-02 12:05:44 +0900

    [Update] findAll 메서드 변경

--- market/src/main/java/com/free/market/item/service/ItemService.java
+++ market/src/main/java/com/free/market/item/service/ItemService.java
@@ -1,5 +1,8 @@
 package com.free.market.item.service;
 
+import com.free.market.common.dto.SearchDto;
+import com.free.market.common.paging.Pagination;
+import com.free.market.common.paging.PagingResponse;
 import com.free.market.item.domain.Item;
 import com.free.market.item.domain.ItemSaveForm;
 import com.free.market.item.domain.ItemUpdateForm;
@@ -8,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -21,8 +25,20 @@ public class ItemService {
      * 상품 목록
      * @return List<Item>
      */
-    public List<Item> findAll() {
-        return itemMapper.findAll();
+    public PagingResponse<Item> findAll(SearchDto params) {
+        // 조건에 해당하는 데이터가 없는 경우, 응답 데이터에 비어있는 리스트와 null을 담아 반환
+        int count = itemMapper.count(params);
+        if(count < 1) {
+            return new PagingResponse<>(Collections.emptyList(), null);
+        }
+
+        // pagination 객체를 생성해서 페이지 정보 계산 후 SearchDto 타입의 객체인 params에 계산된 페이지 정보 저장
+        Pagination pagination = new Pagination(count, params);
+        params.setPagination(pagination);
+
+        // 계산된 페이지 정보의 일부(limitStart, recordSize)를 기준으로 리스트 데이터 조회 후 응답 데이터 반환
+        List<Item> list = itemMapper.findAll(params);
+        return new PagingResponse<>(list, pagination);
     }
 
     /**
