c4e8e30aea5a643b3433777965382c8d9ad10dd1
2017tmxkxm <2017tmxkxm@gmail.com>
2024-03-03 10:18:49 +0900

    [Update] 페이징 > edit 수정중

--- market/src/main/java/com/free/market/item/controller/ItemController.java
+++ market/src/main/java/com/free/market/item/controller/ItemController.java
@@ -10,9 +10,11 @@ import com.free.market.item.domain.Item;
 import com.free.market.item.domain.ItemSaveForm;
 import com.free.market.item.domain.ItemUpdateForm;
 import com.free.market.item.service.ItemService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,6 +48,7 @@ public class ItemController {
     @GetMapping
     public String items(@ModelAttribute(name = "params") SearchDto params, Model model) {
         PagingResponse<Item> response = itemService.findAll(params);
+        log.info("params={}", params);
         model.addAttribute("response", response);
         return "item/items";
     }
@@ -81,15 +85,28 @@ public class ItemController {
 
     @ResponseBody
     @GetMapping("/{itemId}/delete")
-    public String delete(@PathVariable(name = "itemId") Long itemId) {
+    @Transactional
+    public Map<String, Object> delete(@PathVariable(name = "itemId") Long itemId, @ModelAttribute(name = "params") SearchDto params) {
+
+        log.info("fileId={}", params.getFileId());
+
+        Map<String, Object> paramsMap = new HashMap<>();
+        paramsMap.put("page", params.getPage());
+        paramsMap.put("recordSize", params.getRecordSize());
+        paramsMap.put("pageSize", params.getPageSize());
+        paramsMap.put("itemName", params.getItemName());
+        paramsMap.put("price", params.getPrice());
+
         Item item = itemService.findById(itemId);
         if(item != null) {
+            fileService.deleteAllFileByIds(params.getFileId());
             itemService.delete(itemId);
+            paramsMap.put("message", "삭제되었습니다");
         } else {
-            return "잘못된 요청입니다. 해당 게시물은 없습니다 " + itemId;
+            paramsMap.put("message", "잘못된 요청입니다. 해당 게시물은 없습니다 " + itemId);
         }
 
-        return "삭제되었습니다.";
+        return paramsMap;
     }
 
     @GetMapping("/{itemId}/edit")
@@ -99,15 +116,23 @@ public class ItemController {
         return "item/editForm";
     }
 
-    @PostMapping("/{itemId}/edit")
+    @PostMapping(value = "/{itemId}/edit")
+    @Transactional
     public String edit(@PathVariable(name = "itemId") Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form
+                        , HttpServletRequest request
                         , BindingResult bindingResult
                         , RedirectAttributes redirectAttributes) throws IOException {
+
+        String url = request.getHeader("REFERER");
+
+        String params = url.substring(url.indexOf("?") + 1);
 
         if(bindingResult.hasErrors()) {
             return "item/editForm";
         }
 
+        log.info("form={}", form.toString());
+        log.info("files={}", form.getFiles());
         Long updateItemId = itemService.update(form);
 
         // 파일 업로드 (to disk)
@@ -125,7 +150,48 @@ public class ItemController {
         // 파일 삭제 (from database)
         fileService.deleteAllFileByIds(form.getRemoveFileIds());
 
-        redirectAttributes.addAttribute("itemId", updateItemId);
-        return "redirect:/item/{itemId}";
+        //http://localhost:8080/item/21?page=1&recordSize=8&pageSize=10&itemName=%ED%8E%98%EC%9D%B4%EC%A7%95&price=
+        log.info("return value={}", updateItemId + "?" + params);
+        // 20?page=1&recordSize=8&pageSize=10&itemName=29&price=
+        //return updateItemId + "?" + params;
+
+        return "redirect:/item/" + updateItemId + "?" + params;
+    }
+
+    @ResponseBody
+    //@PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable(name = "itemId") Long itemId, @Validated @ModelAttribute("item") ItemUpdateForm form
+            , HttpServletRequest request
+            , BindingResult bindingResult) throws Exception {
+
+        String url = request.getHeader("REFERER");
+
+        String params = url.substring(url.indexOf("?") + 1);
+
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "item/editForm";
+        }
+
+        // 파일 업로드 (to disk)
+        List<FileRequest> uploadFiles = fileUtils.uploadFiles(form.getFiles());
+
+        // 파일 정보 저장 (to database)
+        fileService.saveFile(form.getId(), uploadFiles);
+
+        // 삭제할 파일 정보 조회 (from database)
+        List<FileResponse> deleteFiles = fileService.findAllFileByIds(form.getRemoveFileIds());
+
+        // 파일 삭제 (from disk)
+        fileUtils.deleteFiles(deleteFiles);
+
+        // 파일 삭제 (from database)
+        fileService.deleteAllFileByIds(form.getRemoveFileIds());
+
+        Long updatedItemId = itemService.update(form);
+        log.info("form={}", form.toString());
+
+
+        return updatedItemId + "?" + params;
     }
 }
