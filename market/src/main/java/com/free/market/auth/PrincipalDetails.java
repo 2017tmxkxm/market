a166bb9c132f7f595a635e2fa88932c5fdb9fa22
2017tmxkxm <2017tmxkxm@gmail.com>
2024-03-08 17:14:35 +0900

    [Create] 사용자 정보를 담는 클래스 생성

---
+++ market/src/main/java/com/free/market/auth/PrincipalDetails.java
@@ -0,0 +1,56 @@
+package com.free.market.auth;
+
+import com.free.market.member.domain.MemberRequest;
+import com.free.market.member.domain.MemberResponse;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+// 사용자의 정보를 담는 인터페이스
+public class PrincipalDetails implements UserDetails {
+    private final MemberResponse memberResponse;
+
+    // 일반 로그인
+    public PrincipalDetails(MemberResponse memberResponse) {
+        System.out.println("PrincipalDetails.PrincipalDetails");
+        this.memberResponse = memberResponse;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getPassword() {
+        return memberResponse.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return memberResponse.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
