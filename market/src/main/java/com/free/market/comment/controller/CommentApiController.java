206132991f13a32770ca51e8562937006cfd0155
2017tmxkxm <2017tmxkxm@gmail.com>
2024-03-20 18:18:04 +0900

    [Update] 댓글 상세정보, 수정 메소드 추가

--- market/src/main/java/com/free/market/comment/controller/CommentApiController.java
+++ market/src/main/java/com/free/market/comment/controller/CommentApiController.java
@@ -4,6 +4,7 @@ import com.free.market.comment.domain.CommentRequest;
 import com.free.market.comment.domain.CommentResponse;
 import com.free.market.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,5 +26,18 @@ public class CommentApiController {
     @GetMapping("/item/{itemId}/comments")
     public List<CommentResponse> findAllComment(@PathVariable(name = "itemId") Long itemId) {
         return commentService.findAll(itemId);
+    }
+
+    // 댓글 상세정보 조회
+    @GetMapping("/item/{itemId}/comments/{id}")
+    public CommentResponse findCommentById(@PathVariable(name = "itemId") Long itemId, @PathVariable(name = "id") Long id) {
+        return commentService.findById(id);
+    }
+
+    // 기존 댓글 수정
+    @PatchMapping("/item/{itemId}/comments/{id}")
+    public CommentResponse updateComment(@PathVariable(name = "itemId") Long itemId, @PathVariable(name = "id") Long id, @RequestBody CommentRequest params) {
+        commentService.update(params);
+        return commentService.findById(id);
     }
 }
